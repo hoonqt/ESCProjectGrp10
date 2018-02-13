dc2983d2fc2075ac9b358bfd1b78667c1404aa9b
hoonqt <qitai_hoon@mymail.sutd.edu.sg>
2018-02-13 21:11:55 +0800

    First push

---
+++ allDataInterface.java
@@ -0,0 +1,21 @@
+package com.example.trialesc;
+
+public interface allDataInterface {
+
+    public void getQuestions(String sessionId,String questionNo);
+
+    public void pushQuestions();
+
+    public void submitAnswer();
+
+    //For teachers
+    public void openSession();
+    //For teachers
+    public void closeSession();
+
+    public void getLearningData(Integer weekno);
+
+    public void setLearningData();
+    //For students to key in their session code
+    public void setSession(String session);
+}
