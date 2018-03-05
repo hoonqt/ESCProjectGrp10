d120b646311f1cea8474b25bb3032c216486ede0
hoonqt <qitai_hoon@mymail.sutd.edu.sg>
2018-03-05 19:15:47 +0800

    Questions for activities in progress

---
+++ app/src/main/java/com/example/cindy/esc_50005/activityQuestion/ActivityQuestion.java
@@ -0,0 +1,16 @@
+package com.example.cindy.esc_50005.activityQuestion;
+
+import java.util.List;
+
+/**
+ * Created by hoonqt on 5/3/18.
+ */
+
+public class ActivityQuestion {
+
+    String question;
+    List<String> options;
+    int correctOption;
+    String author;
+
+}
