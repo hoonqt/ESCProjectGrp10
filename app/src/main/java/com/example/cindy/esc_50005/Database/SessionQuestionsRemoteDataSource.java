58a6049d6375dcb4f1659795ecd25178d0566264
hoonqt <qitai_hoon@mymail.sutd.edu.sg>
2018-03-10 21:59:28 +0800

    Changed time

--- app/src/main/java/com/example/cindy/esc_50005/Database/SessionQuestionsRemoteDataSource.java
+++ app/src/main/java/com/example/cindy/esc_50005/Database/SessionQuestionsRemoteDataSource.java
@@ -172,7 +172,7 @@ public class SessionQuestionsRemoteDataSource implements SessionQuestionsDataSou
         getQuestionsList(sessionCode);
 
         try {
-            TimeUnit.MILLISECONDS.sleep(15);
+            TimeUnit.MILLISECONDS.sleep(50);
         }
 
         catch (InterruptedException ex) {
