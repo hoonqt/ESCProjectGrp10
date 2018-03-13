10e76197a4722b6f1612d67707f9f2056e29f129
Jiawen <tan.jia.wen96@gmail.com>
2018-03-14 03:07:00 +0800

    Added Faq usecase with database

--- app/src/main/java/com/example/cindy/esc_50005/UI/Course/FAQ/FaqAdapter.java
+++ app/src/main/java/com/example/cindy/esc_50005/UI/Course/FAQ/FaqAdapter.java
@@ -3,36 +3,35 @@ package com.example.cindy.esc_50005.UI.Course.FAQ;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.cindy.esc_50005.Database.FAQ.Faq;
 import com.example.cindy.esc_50005.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by cindy on 27/11/2017.
  */
 
 public class FaqAdapter extends RecyclerView.Adapter<FaqAdapter.FaqViewHolder> {
-    private FaqPresenter.FaqJsonData[] dataInClass;
+
+    public static final String TAG = "FaqAdapter";
+
+    private ArrayList<Faq> mFaqList;
+    private FaqItemListener mFaqItemListener;
 
     private static int viewHolderCount = 0;
-    Context parentContext;
 
-    <T> FaqAdapter(Context context, T data){
-        this.parentContext = context;
-        final Class<FaqPresenter.FaqJsonData[]> faqJsonDataType = FaqPresenter.FaqJsonData[].class;
-        final FaqPresenter.FaqJsonData[] instance = convertInstanceOfObject(data, faqJsonDataType);
-        dataInClass=instance;
-    }
-
-    public static <T> T convertInstanceOfObject(Object o, Class<T> clazz) {
-        try {
-            return clazz.cast(o);
-        } catch(ClassCastException e) {
-            return null;
-        }
+    public FaqAdapter(ArrayList<Faq> faqList, FaqItemListener itemListener){
+        this.mFaqList = faqList;
+        this.mFaqItemListener = itemListener;
     }
 
 
@@ -43,15 +42,17 @@ public class FaqAdapter extends RecyclerView.Adapter<FaqAdapter.FaqViewHolder> {
     //indicates how many list objects it has
 
     public int getItemCount() {
-        return dataInClass.length;
+        if (mFaqList == null) {
+            return 0;
+        } else {
+            return mFaqList.size();
+        }
     }
 
-
-    public FaqAdapter.FaqViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
+    public FaqViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         int layoutIDForListItem = R.layout.faq_recycler;
-        LayoutInflater inflater = LayoutInflater.from(parentContext);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         boolean shouldAttachToParentImmediately = false;
 
         //java object of layout
@@ -62,39 +63,46 @@ public class FaqAdapter extends RecyclerView.Adapter<FaqAdapter.FaqViewHolder> {
         return faqViewHolder;
     }
 
-
     class FaqViewHolder extends RecyclerView.ViewHolder implements  View.OnClickListener {
-        TextView question;
-        TextView answer;
 
+        TextView tv_question;
+        TextView tv_answer;
+        TextView tv_upvote;
+        TextView tv_time;
+        Button btn_upvote;
 
         FaqViewHolder(View v) {
 
             super(v);
             //we need the v object because the view contains the references to the widgets that we need
-            question = (TextView) v.findViewById(R.id.item_question);
-            answer = (TextView) v.findViewById(R.id.item_answer);
+            tv_question = (TextView) v.findViewById(R.id.faq_tv_question);
+            tv_answer = (TextView) v.findViewById(R.id.faq_tv_answer);
+            tv_upvote = (TextView) v.findViewById(R.id.faq_tv_upvote);
+            tv_time = (TextView) v.findViewById(R.id.faq_tv_time);
+            btn_upvote = (Button) v.findViewById(R.id.faq_btn_upvote);
 
             v.setOnClickListener(this);
+            btn_upvote.setOnClickListener(this);
 
         }
 
         public void bind(int position) {
 
-            question.setText(dataInClass[position]._question);
-//            answer.setText(dataInClass[position]._answers);
+            Faq faq = mFaqList.get(position);
+            tv_question.setText(faq.getQuestion());
+            tv_answer.setText(faq.getAnswer());
+            tv_upvote.setText(String.valueOf(faq.getUpvotes()));
+            tv_time.setText(faq.getAuthor() + ", " + faq.getDate());
         }
 
         @Override
         public void onClick(View v) {
-            int clickedPosition = getAdapterPosition();
-            AlertDialog.Builder builder = new AlertDialog.Builder(parentContext);
 
-            String question = dataInClass[clickedPosition]._question;
-            builder.setMessage("Question: " + question);
+            if (v.getId() == btn_upvote.getId()) {
+                Toast.makeText(v.getContext(), "ITEM PRESSED = " + String.valueOf(getAdapterPosition()), Toast.LENGTH_SHORT).show();
+                mFaqItemListener.onUpvoteClick(mFaqList.get(getAdapterPosition()));
+            }
 
-            AlertDialog alertDialog = builder.create();
-            alertDialog.show();
         }
     }
 }
