6d76513e1b0f05c2e2beb5646af9340fcbc55da3
Kenneth Chua <kennethchuahwachong@hotmail.com>
2018-03-14 23:49:07 +0800

    Trying to port progress to MVP in comments

--- app/src/main/java/com/example/cindy/esc_50005/UI/Course/FAQ/ProgressContract.java
+++ app/src/main/java/com/example/cindy/esc_50005/UI/Course/FAQ/ProgressContract.java
@@ -3,6 +3,8 @@ package com.example.cindy.esc_50005.UI.Course.FAQ;
 import com.example.cindy.esc_50005.UI.Base.BasePresenter;
 import com.example.cindy.esc_50005.UI.Base.BaseView;
 
+import java.util.ArrayList;
+
 /**
  * Created by 1002215 on 12/3/18.
  */
@@ -10,6 +12,7 @@ public interface ProgressContract {
 
     interface View extends BaseView<Presenter> {
         <T> void showProgress(T data);
+//        void showProgress(ArrayList<Double> scoreList);
         void showAverage();
     }
 
