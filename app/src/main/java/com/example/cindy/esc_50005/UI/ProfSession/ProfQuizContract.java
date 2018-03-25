105300d641987c10c2fcd21b342aeb0a9ceaf8f0
hoonqt <qitai_hoon@mymail.sutd.edu.sg>
2018-03-25 12:03:28 +0800

    Prof side in progress

---
+++ app/src/main/java/com/example/cindy/esc_50005/UI/ProfSession/ProfQuizContract.java
@@ -0,0 +1,30 @@
+package com.example.cindy.esc_50005.UI.ProfSession;
+
+import com.example.cindy.esc_50005.Database.Database.SessionQuestionsDO;
+import com.example.cindy.esc_50005.UI.Base.BasePresenter;
+import com.example.cindy.esc_50005.UI.Base.BaseView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hoonqt on 25/3/18.
+ */
+
+public interface ProfQuizContract {
+
+    interface View extends BaseView<Presenter> {
+        void showQuizes();
+        void showAddedQuiz(ArrayList<SessionQuestionsDO> questionsList);
+        void showNoQuiz();
+        void showLoadQuizError();
+//        void questionsLoaded();
+    }
+
+    interface Presenter extends BasePresenter {
+        void loadQuizes();
+        void addNewQuiz(String question);
+        void processEmptyQuiz();
+
+    }
+
+}
---
+++ app/src/main/java/com/example/cindy/esc_50005/UI/ProfSession/ProfQuizPresenter.java
@@ -0,0 +1,8 @@
+package com.example.cindy.esc_50005.UI.ProfSession;
+
+/**
+ * Created by hoonqt on 25/3/18.
+ */
+
+public class ProfQuizPresenter {
+}
