80e3512e632c4ad991ac8c7230e3a4f2e0cbb605
hoonqt <qitai_hoon@mymail.sutd.edu.sg>
2018-04-01 23:53:22 +0800

    Testing in progress

---
+++ app/src/androidTest/java/com/example/esc_50005/SessionQnsTest.java
@@ -0,0 +1,55 @@
+package com.example.esc_50005;
+
+
+import android.content.Context;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.example.esc_50005.UI.DataAdder.AddDatahere;
+import com.example.esc_50005.UI.ProfSession.Contracts.QuizProfContract;
+import com.example.esc_50005.UI.ProfSession.Presenters.ActivityProfPresenter;
+import com.example.esc_50005.UI.Session.Main.SessionActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class SessionQnsTest {
+
+
+    @Mock
+    private QuizProfContract.View mQuizView;
+
+    @Mock
+    private Context mockApplicationContext;
+
+    @Mock
+    private SessionActivity mockSessionActivity;
+
+    private ActivityProfPresenter mProfPresenter;
+
+    private AddDatahere sessionActivity;
+
+    @Before
+    public void setupQuizPresenter() {
+        MockitoAnnotations.initMocks(SessionQnsTest.class);
+        AWSMobileClient.getInstance().initialize(sessionActivity).execute();
+
+    }
+
+    @Test
+    public void createPresenter() {
+        mProfPresenter = new ActivityProfPresenter(mQuizView);
+        verify(mQuizView).setPresenter(mProfPresenter);
+
+    }
+
+
+}
--- app/src/main/java/com/example/esc_50005/UI/ProfSession/Presenters/ProfQuizPresenter.java
+++
@@ -1,8 +0,0 @@
-package com.example.esc_50005.UI.ProfSession.Presenters;
-
-/**
- * Created by hoonqt on 27/3/18.
- */
-
-public class ProfQuizPresenter {
-}
