405cd94915c07f87e9b5eba2f2fb3992204ef745
hoonqt <qitai_hoon@mymail.sutd.edu.sg>
2018-04-02 23:14:45 +0800

    Added database

--- app/src/main/java/com/example/esc_50005/Database/Progress/ProgressRemoteDataSource.java
+++ app/src/main/java/com/example/esc_50005/Database/Progress/ProgressRemoteDataSource.java
@@ -57,6 +57,51 @@ public class ProgressRemoteDataSource implements ProgressDataSource {
 
     }
 
+    public ArrayList<QuizScores4DO> getScoresallStudents(final String subjectCode, final String sessionID) {
+
+        progressArrayList = new ArrayList<QuizScores4DO>();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                QuizScores4DO scores = new QuizScores4DO();
+                scores.setCourseID(subjectCode);
+
+                Condition rangeKeyCondition = new Condition()
+                        .withComparisonOperator(ComparisonOperator.CONTAINS)
+                        .withAttributeValueList(new AttributeValue().withS(sessionID));
+
+
+                DynamoDBQueryExpression queryExpression = new DynamoDBQueryExpression()
+                        .withHashKeyValues(scores)
+                        .withRangeKeyCondition("StudentIDSessionID",rangeKeyCondition)
+                        .withConsistentRead(false);
+
+                PaginatedList<QuizScores4DO> result = dynamoDBMapper.query(QuizScores4DO.class, queryExpression);
+
+                for (QuizScores4DO score : result) {
+                    progressArrayList.add(score);
+                    Log.i("scores in prds","scores: " + score.getScore().toString());
+                }
+
+            }
+        });
+
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        Log.i("Progresslist in prds", "progressList" + progressArrayList.toString());
+
+        return progressArrayList;
+
+    }
+
 
     public ArrayList<QuizScores4DO> getScores(final String subjectCode, final String studentID) {
 
