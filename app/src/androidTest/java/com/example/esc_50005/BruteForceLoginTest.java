33fcbb6425fec2c9c78d959203cc55a30d93c6c3
Jia Wen <tan.jia.wen96@gmail.com>
2018-04-04 08:16:00 +0800

    Added tests for faq and brute force login

--- app/src/androidTest/java/com/example/esc_50005/BruteForceLoginTest.java
+++ app/src/androidTest/java/com/example/esc_50005/BruteForceLoginTest.java
@@ -65,7 +65,7 @@ public class BruteForceLoginTest {
         for (int i = 0; i < passwords.size(); i++) {
             onView(withId(R.id.email)).perform(typeText("corgi101"));
             onView(withId(R.id.password)).perform(typeText(passwords.get(i)));
-            onView(withId(R.id.email_sign_in_button)).perform(click());
+            onView(withId(R.id.login_button)).perform(click());
             onView(withText("Your account has been locked out, kindly contact your adminstrator."))
                     .inRoot(isDialog())
                     .check(matches(isDisplayed()));
