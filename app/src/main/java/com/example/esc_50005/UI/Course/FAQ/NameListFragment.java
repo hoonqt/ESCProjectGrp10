f54ba7c2f41cc429443405a48bcb952fe0fcba63
Kenneth Chua <kennethchuahwachong@hotmail.com>
2018-04-06 16:14:25 +0800

    Fix crashing of empty namelist

--- app/src/main/java/com/example/esc_50005/UI/Course/FAQ/NameListFragment.java
+++ app/src/main/java/com/example/esc_50005/UI/Course/FAQ/NameListFragment.java
@@ -2,10 +2,13 @@ package com.example.esc_50005.UI.Course.FAQ;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,6 +17,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 import com.example.esc_50005.R;
@@ -52,6 +56,7 @@ public class NameListFragment extends Fragment implements ProgressContract.View 
     private SharedPreferences userInformation;
     private NameListAdapter mNameListAdapter;
     private String courseId;
+    private FrameLayout frameLayout;
 
     public NameListFragment() {
         // Required empty public constructor
@@ -88,7 +93,8 @@ public class NameListFragment extends Fragment implements ProgressContract.View 
         nameListRecycler.setLayoutManager(new LinearLayoutManager(this.getActivity()));
 
         userInformation = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
-        courseId = userInformation.getString("CurrentCourseActivity","");
+        courseId = userInformation.getString(getString(R.string.current_course_activity),"");
+        frameLayout = (FrameLayout) view.findViewById(R.id.name_list_fl);
 
         swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.name_list_swipe);
         swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -110,6 +116,22 @@ public class NameListFragment extends Fragment implements ProgressContract.View 
     }
 
     public void showNoName() {
+
+        ArrayList<String> nameList = new ArrayList<>();
+        ArrayList<String> studentIdsList = new ArrayList<>();
+        ArrayList<Double> avg = new ArrayList<>();
+        mNameListAdapter = new NameListAdapter(nameList, studentIdsList, avg, mItemListener);
+        nameListRecycler.setAdapter(mNameListAdapter);
+//            Snackbar snackbar = Snackbar
+//                    .make(frameLayout, "No Connection", Snackbar.LENGTH_LONG)
+//                    .setAction("Retry", new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View view) {
+//                            mPresenter.loadNames();
+//                        }
+//                    });
+//            snackbar.show();
+//            snackbar.setActionTextColor(Color.WHITE);
     }
 
     public void showLoadNameError() {
--- app/src/main/java/com/example/esc_50005/UI/Course/FAQ/ProgressStudentFragment.java
+++ app/src/main/java/com/example/esc_50005/UI/Course/FAQ/ProgressStudentFragment.java
@@ -115,11 +115,11 @@ public class ProgressStudentFragment extends Fragment implements ProgressContrac
 
         private void setData(ArrayList<Double> scoreList){
         ArrayList<BarEntry> yVals = new ArrayList<BarEntry>();
-            Log.i("zain", "scoreList size1 " + scoreList.size());
+            Log.i("setData", "scoreList size1 " + scoreList.size());
         for(int i=0; i<scoreList.size(); i++){
             double score = scoreList.get(i);
-            Log.i("zain", "score " + score);
-            float value = (float) score/5*100;
+            Log.i("setData", "score " + score);
+            float value = (float) score;
             yVals.add(new BarEntry(i,value));
         }
 
@@ -152,7 +152,7 @@ public class ProgressStudentFragment extends Fragment implements ProgressContrac
         mChart.setMaxVisibleValueCount(40);
 
         final String[] ds = new String[scoreList.size()];
-        Log.i("zain", "scoreList size " + scoreList.size());
+        Log.i("ShowProgress", "scoreList size " + scoreList.size());
         for(int i=0; i<scoreList.size();i++){
             ds[i]= "Quiz " + (i+1);
         }
@@ -160,26 +160,6 @@ public class ProgressStudentFragment extends Fragment implements ProgressContrac
 
         XAxis xval = mChart.getXAxis();
         xval.setDrawLabels(false);
-
-//        xval.setValueFormatter(new MyAxisValueFormatter() {
-//            @Override
-//            public String getFormattedValue(float value, AxisBase axis) {
-//                Log.i("zain", "value " + value);
-////                if(value>scoreList.size()){
-////                    return ds[Math.round(value)-1];
-////                }else {
-////                    return ds[Math.round(value)];
-////                }
-//                return ds[1];
-//
-//
-//            }
-//
-////            @Override
-////            public int getDecimalDigits() {
-////                return 0;
-////            }
-//        });
 
         setData(scoreList);
         mChart.setFitBars(false);
@@ -199,6 +179,11 @@ public class ProgressStudentFragment extends Fragment implements ProgressContrac
 
     }
 
+    @Override
+    public void showNoName() {
+
+    }
+
     // TO BE REMOVED (cant remove yet due to QuestionsFragment using it)
     public class FaqJsonData {
 
@@ -208,110 +193,3 @@ public class ProgressStudentFragment extends Fragment implements ProgressContrac
 
     }
 }
-//public class ProgressStudentFragment extends Fragment implements ProgressContract.View {
-//
-//    private SessionQuestionsRemoteDataSource progressRepository= new SessionQuestionsRemoteDataSource();
-//    private ProgressContract.Presenter mPresenter = new ProgressPresenter(progressRepository,this);
-//    private BarChart mChart;
-//
-//    public ProgressStudentFragment() {
-//        // Required empty public constructor
-//    }
-//
-//    public static ProgressStudentFragment newInstance() {
-//        return new ProgressStudentFragment();
-//    }
-//    @Override
-//    public void onCreate(@Nullable Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//    }
-//
-//    @Override
-//    public void onResume() {
-//        super.onResume();
-//        mPresenter.start();
-//    }
-//
-//    @Override
-//    public void setPresenter(@NonNull ProgressContract.Presenter presenter) {
-//        mPresenter = checkNotNull(presenter);
-//    }
-//
-//    @Nullable
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        View view = inflater.inflate(R.layout.my_progress_main, container, false);
-//        mChart = (BarChart)view.findViewById(R.id.chart1);
-//
-//        return view;
-//    }
-//
-//    private void setData(int count){
-//        ArrayList<BarEntry> yVals = new ArrayList<BarEntry>();
-//
-//        for(int i=0; i<count; i++){
-//            float value = (i * 20 );
-//            yVals.add(new BarEntry(i,value));
-//        }
-//
-//        BarDataSet set = new BarDataSet(yVals, "(Quizzes)");
-//        set.setDrawIcons(false);
-//        set.setColors(ColorTemplate.LIBERTY_COLORS);
-//        //Show value on top of each bar
-//        set.setDrawValues(true);
-//        set.setValueTextSize(15);
-//
-//        BarData data = new BarData(set);
-//        data.setValueFormatter(new MyValueFormatter());
-//        mChart.setData(data);
-//        mChart.invalidate();
-//        mChart.animateY(500);
-//    }
-//
-//
-//
-//    @Override
-//    public <T> void showProgress(T data) {
-//
-//        mChart.getDescription().setEnabled(false);
-//        mChart.setMaxVisibleValueCount(40);
-//
-//        final String[] ds = new String[5];
-//        ds[0]="Quiz 1";
-//        ds[1]="Quiz 2";
-//        ds[2]="Quiz 3";
-//        ds[3]="Quiz 4";
-//        ds[4]="Quiz 5";
-//
-//
-//        XAxis xval = mChart.getXAxis();
-//        xval.setDrawLabels(true);
-//        xval.setValueFormatter(new MyAxisValueFormatter() {
-//            @Override
-//            public String getFormattedValue(float value, AxisBase axis) {
-//                Log.i("zain", "value " + value);
-//                return ds[Math.round(value)];
-//            }
-//
-////            @Override
-////            public int getDecimalDigits() {
-////                return 0;
-////            }
-//        });
-//
-//        setData(5);
-//        mChart.setFitBars(true);
-//
-////        SessionQuestionsRemoteDataSource session= new SessionQuestionsRemoteDataSource();
-////        session.addQuestion("What is the difference between Observer and Strategy Design Pattern?","111");
-//
-//    }
-//
-//    @Override
-//    public void showAverage() {
-//
-//    }
-//
-//}
