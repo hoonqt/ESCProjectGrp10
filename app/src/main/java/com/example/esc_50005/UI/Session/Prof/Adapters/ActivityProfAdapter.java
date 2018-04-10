8f2cb86bdc526d9f9c00758a6e1fc7c0fdbab6ec
hoonqt <qitai_hoon@mymail.sutd.edu.sg>
2018-04-10 09:23:10 +0800

    Prof side still in the works.

--- app/src/main/java/com/example/esc_50005/UI/Session/Prof/Adapters/ActivityProfAdapter.java
+++ app/src/main/java/com/example/esc_50005/UI/Session/Prof/Adapters/ActivityProfAdapter.java
@@ -1,6 +1,8 @@
 package com.example.esc_50005.UI.Session.Prof.Adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -10,11 +12,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.PopupMenu;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import com.example.esc_50005.Database.Quizstuff.QuizQuestions2DO;
+import com.example.esc_50005.Database.Quizstuff.QuizRemoteDataSource;
 import com.example.esc_50005.R;
 import com.example.esc_50005.UI.Session.Main.SessionActivity;
 import com.example.esc_50005.UI.Session.Prof.SideScreens.EditQnListFrag;
@@ -26,10 +30,11 @@ import java.util.ArrayList;
  * Created by hoonqt on 25/3/18.
  */
 
-public class ActivityProfAdapter extends RecyclerView.Adapter<ActivityProfAdapter.QuizViewHolder> {
+public class ActivityProfAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private ArrayList<QuizQuestions2DO> dataset;
     private ArrayList<String> names;
+    private ArrayList<QuizQuestions2DO> smallerdataset;
     private static int viewHolderCount = 0;
     private Context context;
     SharedPreferences sharedPreferences;
@@ -43,10 +48,12 @@ public class ActivityProfAdapter extends RecyclerView.Adapter<ActivityProfAdapte
 
             if (dataset.get(i).getIsItQn()) {
                 names.add(dataset.get(i).getQuizNameQnID().split(" ")[0]+ " " +dataset.get(i).getQuizNameQnID().split(" ")[1]);
+                smallerdataset.add(dataset.get(i));
             }
 
             else if (!(names.contains(dataset.get(i).getQuizNameQnID().split(" ")[0]+ " " +dataset.get(i).getQuizNameQnID().split(" ")[1]))) {
                 names.add(dataset.get(i).getQuizNameQnID().split(" ")[0]+ " " +dataset.get(i).getQuizNameQnID().split(" ")[1]);
+                smallerdataset.add(dataset.get(i));
             }
         }
 
@@ -55,12 +62,18 @@ public class ActivityProfAdapter extends RecyclerView.Adapter<ActivityProfAdapte
 
     @Override
     public int getItemViewType(int position) {
-        return super.getItemViewType(position);
+
+        if (smallerdataset.get(position).getIsItQn()) {
+            return 0;
+        }
+
+        else return 1;
     }
 
     // Create new views (invoked by the layout manager)
     @Override
-    public ActivityProfAdapter.QuizViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
         int layoutIDForListItem = R.layout.profquiz_recycler;
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         boolean shouldAttachToParentImmediately = false;
@@ -69,16 +82,35 @@ public class ActivityProfAdapter extends RecyclerView.Adapter<ActivityProfAdapte
 
         View view = inflater.inflate(layoutIDForListItem,parent,shouldAttachToParentImmediately);
 
-        QuizViewHolder quizViewHolder = new QuizViewHolder(view);
         viewHolderCount++;
 
-        return quizViewHolder;
+        switch (viewType) {
+            case 0: return new QuestionViewHolder(view);
+
+            case 1: return new QuizViewHolder(view);
+
+            default: return null;
+
+        }
+
+
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ActivityProfAdapter.QuizViewHolder holder, int position) {
-        holder.bind(position);
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        switch (holder.getItemViewType()) {
+            case 0:
+                QuestionViewHolder viewholder = (QuestionViewHolder)holder;
+                viewholder.bind(position);
+
+            case 1:
+                QuizViewHolder viewholder1 = (QuizViewHolder)holder;
+                viewholder1.bind(position);
+
+        }
+
     }
 
     @Override
@@ -158,8 +190,73 @@ public class ActivityProfAdapter extends RecyclerView.Adapter<ActivityProfAdapte
 
     class QuestionViewHolder extends RecyclerView.ViewHolder {
 
+        public TextView mTextView;
+        public TextView lunchPad;
+        public Switch switchUp;
+
         public QuestionViewHolder(View v) {
             super(v);
+            mTextView = (TextView)v.findViewById(R.id.quizname);
+            lunchPad = (TextView) v.findViewById(R.id.moreinfobutton);
+            switchUp = (Switch) v.findViewById(R.id.profquiztoggle) ;
+            lunchPad.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    PopupMenu popup = new PopupMenu(context,lunchPad);
+                    popup.inflate(R.menu.qnoptions_menu);
+                    popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem menuItem) {
+
+                            switch (menuItem.getItemId()) {
+                                case (R.id.editbutton):
+                                    AlertDialog.Builder qnBuilder = new AlertDialog.Builder(context);
+                                    qnBuilder.setTitle("Edit question");
+                                    qnBuilder.setMessage("Enter question");
+                                    final EditText input = new EditText(context);
+
+                                    input.setText(smallerdataset.get(getAdapterPosition()).getQuestion());
+                                    qnBuilder.setView(input);
+
+                                    qnBuilder.setNegativeButton("Submit", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                                            QuizQuestions2DO tobeadded = new QuizQuestions2DO();
+                                            tobeadded.setQuizNameQnID(smallerdataset.get(getAdapterPosition()).getQuizNameQnID());
+                                            tobeadded.setSubjectCodeSessionCode(sharedPreferences.getString("CurrentCourseActivity", null).split(" ")[0]+sharedPreferences.getString("Current Session Id",""));
+                                            tobeadded.setIsItQn(true);
+                                            tobeadded.setQuestion(input.getText().toString());
+
+                                            QuizRemoteDataSource adder = new QuizRemoteDataSource();
+                                            adder.putQuestion(tobeadded);
+
+                                            dialogInterface.dismiss();
+                                        }
+                                    });
+                                    return true;
+
+                                case (R.id.responsesbutton):
+                                    //Goes to new fragment
+                                    return false;
+
+                            }
+                            return false;
+
+                        }
+                    });
+
+                    popup.show();
+                }
+            });
+
+        }
+
+        public void bind(int position) {
+
+            mTextView.setText(names.get(position));
+
         }
     }
 
